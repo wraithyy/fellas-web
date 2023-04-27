6ea3c6af1e12c226953dca86a3bc1d504f6d7241
Daniel Pohořalý <dpohoraly@trask.cz>
2023-04-27 15:01:28 +0200

    obrazky

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,16 @@ import { useEffect } from "react";
 export interface ButtonProps {
   text: React.ReactNode;
   href?: string;
+  white?: boolean;
 }
 
-export const Button = ({ text, href }: ButtonProps) => {
+export const Button = ({ text, href, white }: ButtonProps) => {
   useEffect(() => console.log("asdfasdf"), []);
+  const color = white
+    ? "bg-white hover:bg-gray-700 text-black font-bold py-2 px-4 rounded h-10 flex items-center text-center no-underline ml-3"
+    : "bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded h-10 flex items-center text-center no-underline ml-3";
   return (
-    <a
-      className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded h-10 flex items-center text-center no-underline ml-3"
-      href={href}
-    >
+    <a className={color} href={href}>
       {text}
     </a>
   );
